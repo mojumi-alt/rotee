6dc55d0e0295570f2c7ea10ff65c95231c0ce1e1
Moritz Milla <80782057+mojumi-alt@users.noreply.github.com>
2024-10-21 23:19:10 +0200

    Add some usage examples

--- examples/after_time.sh
+++ examples/after_time.sh
@@ -1,10 +1,28 @@
 #!/bin/bash
 
+trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
+
 {
     while :
     do
         echo $(date) - Something is happening...
         sleep 1
     done
-} 2&>1 | rotee 
+    # Set output and trigger file, truncate on startup, compress archives, keep max 5 archives, check trigger file every .5 seconds
+} | ./rotee -o test.log -t test.trigger -x -c -n 5 -f 0.5 &
 
+{
+    while :
+    do
+        # Rotate log file every 5 seconds
+        # You could also set up a cron job that does this write
+        sleep 5
+        echo "Rotate!"
+        echo 1 &> test.trigger
+
+        # Lazy, if you really need to know when / if the rotate was successful
+        # read the content of the trigger file. 0 means success, 2 means error
+        sleep 1
+        echo Exit code of rotate: $(cat test.trigger)
+    done
+}
